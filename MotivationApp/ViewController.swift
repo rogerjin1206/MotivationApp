0e5860b56b74dd931b744f1b6d4065831d43b877
LEEJAEJIN <jinlee1206@naver.com>
2018-09-28 14:01:43 +0900

    Initial Commit

---
+++ MotivationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotivationApp
+//
+//  Created by Euijae Hong on 28/09/2018.
+//  Copyright © 2018 JAEJIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
